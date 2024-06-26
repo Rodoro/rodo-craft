b0500dedfe85c6f143edee9779bccf3604728479
Rodoro <rodoro.game@gmail.com>
2024-06-26 19:06:10 +0300

    Update structure project

---
+++ frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"8D5gLuO++EnH4batQUtN8qwyQSXRu9zyop3pBaxexa4=\"\n}"
--- src/constants/seo.constants.ts
+++
@@ -1,1 +0,0 @@
-export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }
